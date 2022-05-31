354f5a62121d1b7389e46a6bf7219359fbe843fe
rqphy <raphael.ferreira@hetic.net>
2022-05-31 18:03:55 +0200

    :lipstick: blog post

--- src/Components/BlogPost/BlogPost.js
+++ src/Components/BlogPost/BlogPost.js
@@ -2,11 +2,13 @@ import './BlogPost.css'
 
 const BlogPost = () =>
 {
-  return (
-    <form className="BlogPost">
-      
-    </form>
-  )
+    return (
+        <article className="BlogPost">
+            <p className='BlogPost__title'>Hello World</p>
+            <span className='BlogPost__author'>by: Toto</span>
+            <p className='BlogPost__content'>First Post !</p>
+        </article>
+    )
 }
 
 export default BlogPost
