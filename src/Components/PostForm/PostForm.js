59c00e387962ff5622cb519409d5b3383957e94b
rqphy <raphael.ferreira@hetic.net>
2022-05-31 18:50:47 +0200

    :construction: post form wip

--- src/Components/PostForm/PostForm.js
+++ src/Components/PostForm/PostForm.js
@@ -1,12 +1,24 @@
 import './PostForm.css'
 
+const handlePostSubmit = (_event) =>
+{
+    _event.preventDefault()
+    console.log(_event)
+}
+
 const PostForm = () =>
 {
-  return (
-    <form className="PostForm">
-      
-    </form>
-  )
+
+    return (
+        <div className="PostForm">
+            <h2 className='subtitle'>Create a post</h2>
+            <form onSubmit={handlePostSubmit}>
+                <input className='PostForm__title' type="text" placeholder="Title" name="title" />
+                <textarea type="text" placeholder="Content" name="content" />
+                <input className='cta' type="submit" value="Send"/>
+            </form>
+        </div>
+    )
 }
 
 export default PostForm
